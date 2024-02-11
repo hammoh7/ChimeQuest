040ec82cf3e3db812a9d60a283dcd28aa2926346
hammoh7 <soahammohaadkar@gmail.com>
2024-02-11 13:01:22 +0530

    Libraries

---
+++ lib/conversation.ts
@@ -0,0 +1,64 @@
+import { database } from "./database";
+
+export const Conversations = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  let conversation =
+    (await findConversation(memberOneId, memberTwoId)) ||
+    (await findConversation(memberTwoId, memberOneId));
+
+  if (!conversation) {
+    conversation = await newConversation(memberOneId, memberTwoId);
+  }
+  return conversation;
+};
+
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+  try {
+    return await database.conversation.findFirst({
+      where: {
+        AND: [{ memberOneId: memberOneId }, { memberTwoId: memberTwoId }],
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+  } catch {
+    return null;
+  }
+};
+
+const newConversation = async (memberOneId: string, memberTwoId: string) => {
+  try {
+    return await database.conversation.create({
+      data: {
+        memberOneId,
+        memberTwoId,
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+  } catch {
+    return null;
+  }
+};
